c9ba86939e8e33c5f3960517cea8961369219f47
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-07-12 19:05:57 +0200

    adjust navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,11 @@ export default function Header() {
 				<NavBar />
 
 				<div className="flex flex-col items-center">
-					<h1 className="text-6xl mb-8">A modern publishing platform</h1>
+					<h1 className="text-6xl mb-8 text-center">A modern publishing platform</h1>
 					<p className="mb-12">Grow your audience and build your online brand</p>
-					<div className="flex justify-center mb-32">
-						<button className="bg-white text-primary-light-red mr-2 px-4 py-2 rounded-full">Start for Free</button>
-						<button className="border border-white ml-2 px-4 py-2 rounded-full">Learn More</button>
+					<div className="flex flex-col gap-4 sm:flex-row justify-around lg:justify-center mb-32">
+						<button className="bg-white text-primary-light-red px-4 py-2 rounded-full">Start for Free</button>
+						<button className="border border-white px-4 py-2 rounded-full">Learn More</button>
 					</div>
 				</div>
 			</div>
