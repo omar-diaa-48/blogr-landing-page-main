e2e056ccb013ccf3683f1c75ef5d3b387e5d2908
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-07-12 17:37:36 +0200

    adjust images folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+
 function App() {
 	return (
-		<div>
+		<div className="min-h-full min-w-full px-48">
+
+			<nav className="flex items-center h-32 bg-">
+				<div className="flex items-center mr-4">
+					<button>Product</button>
+					<svg xmlns="http://www.w3.org/2000/svg" width="10" height="7"><path fill="none" stroke="#FFF" strokeWidth="2" opacity=".75" d="M1 1l4 4 4-4" /></svg>
+				</div>
+				<div className="mr-4"><button>Company</button></div>
+				<div className="flex-grow"><button>Connect</button></div>
+
+				<div className="mr-4"><button>Login</button></div>
+				<div><button>Sign Up</button></div>
+			</nav>
+
+			<section></section>
+
+			<div></div>
+
+			<div></div>
+
+			<div></div>
+
+			<footer></footer>
+
 		</div>
 	);
 }
